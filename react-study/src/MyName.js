a07e729e2404a5dee3d92df2cbb566bc71bec823
yeoj1n <yeojinkim94@gmail.com>
2020-01-03 14:45:31 +0900

    클래스형/함수형 컴포넌트 학습

--- react-study/src/MyName.js
+++ react-study/src/MyName.js
@@ -1,6 +1,15 @@
+// 클래스형 컴포넌트
+/*
 import React, { PureComponent } from 'react'
 
 class MyName extends PureComponent {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            
+        }
+    }
 
     render() {
         return (
@@ -8,5 +17,27 @@ class MyName extends PureComponent {
         )
     }
 }
+
+MyName.defaultProps = {
+    subject: '리액트',
+    name: '이름'
+};
+
+export default MyName
+*/
+
+// 함수형 컴포넌트
+import React from 'react'
+
+function MyName({subject}) {
+    return (
+        <div>안녕하세요. 이제부터 <b>{subject}</b>를 공부해봅시다.</div>
+    )
+}
+
+MyName.defaultProps = {
+    subject: '리액트',
+    name: '이름'
+};
 
 export default MyName
