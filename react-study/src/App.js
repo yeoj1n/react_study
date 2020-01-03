3574b58571abc0a7a06bf5849b1ded44da504c70
yeoj1n <yeojinkim94@gmail.com>
2020-01-03 15:54:31 +0900

    리액트 counter 만들어보기, counter 동작하며 리액트 라이프사이클 체크

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import MyName from './MyName';
+//import MyName from './MyName';
+import Counter from './Counter';
 
 
 function App() {
   return (
-    <MyName subject="리액트"/>
+    //<MyName subject="리액트"/>
+    <Counter/>
   );
 }
 
