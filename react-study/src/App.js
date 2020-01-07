f441c9ffe011cc9151074b263dd1ea3289ebe47c
yeoj1n <yeojinkim94@gmail.com>
2020-01-07 11:07:02 +0900

    chapter2.md merge

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 //import MyName from './MyName';
-import Counter from './Counter';
-
+//import Counter from './Counter';
+import PhoneForm from './components/PhoneForm';
 
 function App() {
   return (
     //<MyName subject="리액트"/>
-    <Counter/>
+    // <Counter/>
+    <PhoneForm/>
   );
 }
 
